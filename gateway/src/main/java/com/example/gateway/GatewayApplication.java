7822bb81ec8354573fdc5847ca8341d66e5209f3
DimaS <h@>
2024-12-27 21:07:24 +0400

    добавил статику

--- gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -2,10 +2,19 @@ package com.example.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
 
 @SpringBootApplication
 public class GatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	RouterFunction staticResourceLocator(){
+		return RouterFunctions.resources("/**", new FileSystemResource("D:\\Documents\\TRPO\\gateway\\src\\main\\resources\\static\\dist\\terminal-frontend/"));
 	}
 }
